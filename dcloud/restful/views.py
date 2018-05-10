ab23721d1bcbddc4fdd21019139d42596c47a07f
kairos03 <kairos9603@gmail.com>
2018-05-10 13:56:50 +0900

    fix test code
    kairos03/2018-1-d.cloud#8
    kairos03/2018-1-d.cloud#10

--- dcloud/restful/views.py
+++ dcloud/restful/views.py
@@ -38,14 +38,14 @@ class FileDetail(APIView):
     def get(self, request, pk, format=None):
         file = self.get_object(pk)
         serializer = FileSerializer(file)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, pk, format=None):
         file = self.get_object(pk)
         serializer = FileSerializer(file, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
