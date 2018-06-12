02357f2cdd709739d0ecea2aaf1cb2073747744e
Kangsubeen <hillier@khu.ac.kr>
2018-06-13 05:08:05 +0900

    upload

--- dcloud/website/urls.py
+++ dcloud/website/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     url(r'^accounts/delete_account_success/$', auth_views.delete_account_success, name='delete_account_success'),
 
     # blog
-    url(r'^$', views.home),
-    url(r'^files/', views.file_list, name='file_list'),
-    url(r'^upload/', views.file_upload, name='file_upload'),
+    url(r'^$', views.home, name='home'),
+    url(r'^list/(?P<path>([\w\s가-힣.\`\'\˜\=\+\#\ˆ\@\$\&\-\.\(\)\{\}\;\[\]]*/)*)$', views.file_list, name='file_list'),
+    url(r'^upload/(?P<path>([\w\s가-힣.\`\'\˜\=\+\#\ˆ\@\$\&\-\.\(\)\{\}\;\[\]]*/)*)$', views.file_upload, name='file_upload'),
     url(r'^make_folder/', views.make_folder, name='make_folder'),
 
 ]
--- dcloud/website/views.py
+++ dcloud/website/views.py
@@ -3,7 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.views.generic import FormView
 from restful.models import File
+from django.views import View
+from django.core.files.base import ContentFile
+from django.middleware import csrf
 import requests
+
 
 
 def home(request):
@@ -11,17 +15,24 @@ def home(request):
 
 
 @login_required
-def file_list(request):
+def file_list(request, path):
     cookies = {'sessionid' : request.session.session_key}
-    files = requests.get('http://localhost:8000/restapi/list/', cookies=cookies)
-    return render(request, 'website/file_list.html', files.json())
+    files = requests.get('http://localhost:8000/restapi/list/'+path, cookies=cookies)
+    ret = files.json()
+    ret['path'] = path
+    return render(request, 'website/file_list.html', ret)
 
-def file_upload(request):
+@login_required
+def file_upload(request, path):
+	file = request.FILES.get('file')
 	cookies = {'sessionid' : request.session.session_key}
-	requests.post('http://localhost:8000/restapi/list/',request, cookies=cookies)
-	return redirect('file_list')
+	cookies['csrftoken'] = csrf.get_token(request)
+	headers = {'X-CSRFToken': cookies['csrftoken']}
+	print(cookies)
+	requests.post('http://localhost:8000/restapi/list/', files={'file': file}, headers=headers, cookies=cookies)
+	return redirect('file_list', path=path)
 	
-def make_folder(request):
+def make_folder(request, path):
 	cookies = {'sessionid' : request.session.session_key}
-	files = requests.put('http://localhost:8000/restapi/list/', cookies=cookies)
-	return redirect('file_list')
+	files = requests.put('http://localhost:8000/restapi/list/'+path, cookies=cookies)
+	return redirect('file_list', path=path)
