d432d50e47daa9f319f17cecb137909d3c4c4da2
Kangsubeen <hillier@khu.ac.kr>
2018-06-13 18:38:26 +0900

    make folder

--- dcloud/website/views.py
+++ dcloud/website/views.py
@@ -16,11 +16,11 @@ def home(request):
 
 @login_required
 def file_list(request, path):
-    cookies = {'sessionid' : request.session.session_key}
-    files = requests.get('http://localhost:8000/restapi/list/'+path, cookies=cookies)
-    ret = files.json()
-    ret['path'] = path
-    return render(request, 'website/file_list.html', ret)
+	cookies = {'sessionid' : request.session.session_key}
+	files = requests.get('http://localhost:8000/restapi/list/'+path, cookies=cookies)
+	ret = files.json()
+	ret['path'] = path
+	return render(request, 'website/file_list.html', ret)
 
 @login_required
 def file_upload(request, path):
@@ -28,11 +28,26 @@ def file_upload(request, path):
 	cookies = {'sessionid' : request.session.session_key}
 	cookies['csrftoken'] = csrf.get_token(request)
 	headers = {'X-CSRFToken': cookies['csrftoken']}
-	print(cookies)
-	requests.post('http://localhost:8000/restapi/list/', files={'file': file}, headers=headers, cookies=cookies)
+	requests.post('http://localhost:8000/restapi/list/'+path, files={'file': file}, headers=headers, cookies=cookies)
 	return redirect('file_list', path=path)
 	
 def make_folder(request, path):
+	dir_name = request.POST.get('dir_name')
 	cookies = {'sessionid' : request.session.session_key}
-	files = requests.put('http://localhost:8000/restapi/list/'+path, cookies=cookies)
+	cookies['csrftoken'] = csrf.get_token(request)
+	headers = {'X-CSRFToken': cookies['csrftoken']}
+	print(path)
+	files = requests.put('http://localhost:8000/restapi/list/'+path, headers=headers, cookies=cookies)
 	return redirect('file_list', path=path)
+
+def file_delete(request, path):
+	cookies = {'sessionid' : request.session.session_key}
+	cookies['csrftoken'] = csrf.get_token(request)
+	headers = {'X-CSRFToken': cookies['csrftoken']}
+	requests.delete('http://localhost:8000/restapi/file/'+path, headers=headers, cookies=cookies)
+	return redirect('file_list', path="/".join(path.split("/")[:-2]))
+
+def file_download(request, path):
+	cookies = {'sessionid' : request.session.session_key}
+	requests.get('http://localhost:8000/restapi/file/'+path, cookies=cookies)
+	return redirect('file_list', path="/".join(path.split("/")[:-2]))
